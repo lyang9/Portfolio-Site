4e01527d770981d243477b4aeb4d552d1259c574
Linda Yang <yanglinda24@gmail.com>
2019-03-31 19:34:22 -0400

    Changed navbar and implemented animaimation

--- Linda-site/js/main.js
+++ Linda-site/js/main.js
@@ -157,7 +157,26 @@
    	// fadeout the navigation panel
    	nav.fadeOut();   		
    	     
-  	});
+		});
+		
+
+	/*---------------------------------------------------- */
+		/* Animation Navigation Menu
+		------------------------------------------------------ */
+		$(document).ready(function() {
+			$(document).delegate('.open', 'click', function(event) {
+				$(this).addClass('oppenned');
+				event.stopPropagation();
+			})
+			$(document).delegate('body', 'click', function(event) {
+				$('.open').removeClass('oppenned');
+			})
+			$(document).delegate('.cls', 'click', function(event) {
+				$('.open').removeClass('oppenned');
+				event.stopPropagation();
+			});
+		});
+			
 
 
    /*---------------------------------------------------- */
